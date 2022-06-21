370af3b3cda7c365b8602cd90fa2c255056d56dc
Shubham Srivastava <shubhamsri0807@gmail.com>
2022-06-21 16:12:53 +0530

    shubham

---
+++ index.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const port = process.env.PORT || 8800;
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      `https://api.openweathermap.org/data/2.5/weather?q=Lucknow&units=metric&appid=3b5f7beea1c0e81e645fb60c1bb7320a`
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        
+        // console.log(arrData[0].main.temp);
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+        // console.log(val.main)
+        // console.log(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  } else {
+    res.end("File not found");
+  }
+});
+
+server.listen(port,()=>{
+    console.log("listening on " +port);
+});
